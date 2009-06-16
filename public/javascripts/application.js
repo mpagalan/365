d65e81b99b17bc97b2ace5a8f2689981ef2d8f59
mpagalan <mpagalan@lan-203.office.manila.exist.com>
2009-06-16 20:10:58 +0800

    add date picker and fix main image scroller js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,8 @@
 
+$(window).load(function(){
+  $('#mainContainer').css('visibility', 'visible');
+});
+
 function refigure() {
  var height = $(window).height();
  var width = $(window).width();
@@ -8,22 +12,50 @@ function refigure() {
  $('#mainContainer').css({'width': width, 'height': height});
 }
 
-$(window).ready(function(){
-  refigure();
- $('#mainContainer').css('visibility','visible');
-});
-
-$(window).resize( function (){
-  refigure();
-});
 
 // this is the main image scroller function
-var 
-function mainScroller(){
-  var effects = [50, { easing: 'linear', axis: 'y' }]
- // if (direction == 'up')
+function mainScroller(scrollable){
+  this.scrollable = scrollable;
+  this.effects = [50, { easing: 'linear', axis: 'y' }];
   this.moveUp = scrollerMoveUp;
   this.moveDown = scrollerMoveDown;
   this.move = scrollerMove;
+  this.initialize = scrollerInitialize;
+  this.$current = "";
 
+  function scrollerInitialize(){
+    this.$current = $('div.fullimage:first').eq(0);
+  }
+
+  function scrollerMove(direction, has_effects){
+    var curr;
+    console.log(this.$current);
+    if (direction == "up"){
+      curr = this.$current.prev().eq(0);
+      
+      if (curr.attr("class") != "fullimage") {
+        curr = $('div.fullimage:last').eq(0);
+      }
+    }else if (direction == "down"){
+      curr = this.$current.next().eq(0);
+      
+      if (curr.attr("class") != "fullimage"){
+        curr = $('div.fullimage:first').eq(0);
+      }
+    }else {
+      curr = this.$current ;
+    }
+    console.log(direction);
+    console.log(curr);
+    this.$current = curr;
+    this.scrollable.scrollTo(curr, 30, { easing: 'linear', axis: 'y'});
+  }
+
+  function scrollerMoveUp(has_effects){
+    this.move("up", has_effects);
+  }
+
+  function scrollerMoveDown(has_effects){
+    this.move("down", has_effects);
+  }
 }
