3ee173745b3ea3f533c5a3ebc2f07aa85bf6f00f
mpagalan <mpagalan@lan-203.office.manila.exist.com>
2009-09-09 17:25:17 +0800

    remove pagination on the pages_controler

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   cache_sweeper PageSweeper, :only => [:index]
   
   def index
-    @pages = Page.paginate(:page => params[:page], :include => :comments)
-
+    #@pages = Page.paginate(:page => params[:page], :include => :comments)
+    @pages = Page.find(:all , :include => :comments)
     respond_to do |format|
       format.html #render .html template
       format.js #render .js template
